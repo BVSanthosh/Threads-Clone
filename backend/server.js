82b0ef87b7eff932fd754a5a192cf27c4878415d
BVSanthosh <sb409@st-andrews.ac.uk>
2025-06-14 19:35:00 +0100

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
@@ -9,8 +10,10 @@ import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
-
 connectDB();
+
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +29,12 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
