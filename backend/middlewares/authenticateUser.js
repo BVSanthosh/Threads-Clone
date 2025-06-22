e6d82d213af5cb14190ac0fbded1b4e3632451ae
BVSanthosh <sb409@st-andrews.ac.uk>
2025-06-22 18:08:40 +0100

    fixed middleware

--- backend/middlewares/authenticateUser.js
+++ backend/middlewares/authenticateUser.js
@@ -1,21 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
 const authenticateToken = async (req, res, next) => {
-  try {
-    const token = req.cookies.token;
+    try{
+        const token = req.cookies.token;
 
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorised user!" });
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorised user!" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorised user!" });
+        }
+
+        req.user = user;
+        next();
+    } catch(error) {
+        console.error(`Error in authentiateToken: ${error.message}`);
+        return res.status(500).json({ message: error.message });
     }
+} 
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId).select("-password");
-
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorised user!" });
-    }
-
-    req.user = user;
-    next();
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-}
+export default authenticateToken;
